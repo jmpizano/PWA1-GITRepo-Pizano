ff3d3b60f3b5cfc3d68711ed196baba4f91926a1
jmpizano <jeremiah.pizano@gmail.com>
2015-05-09 17:54:38 -0700

    Flow Chart
    Contains my week 1 flow chart

---
+++ Wk 1/goal1_assign_duel1/js/mainDualOne.js
@@ -0,0 +1,21 @@
+/**
+ * Created by JP on 5/9/15.
+ */
+//Name: Jeremiah Pizano
+//Date: May 9, 2015
+//Class: PWA 1
+
+/* Create variables for player's name, health and damage.
+Create variable for the round number
+
+Create a fight FOR function to keep track of rounds fought and the players health
+in the function also write code to inflict random damage to both characters per round.
+IF round limit exceeds 10 rounds break
+
+Create FOR function to check if there is a winner
+IF Both player's heath are below 1, print no one wins
+ELSE IF player one's health is below 1 print "Player two wins"
+ELSE IF player two's health is below 1 print "Player one wins"
+return results
+
+ */
