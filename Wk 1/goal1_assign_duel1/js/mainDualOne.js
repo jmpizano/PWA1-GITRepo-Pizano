f92fa6f83f4b7a109273b650e394733d9fc7d896
jmpizano <jeremiah.pizano@gmail.com>
2015-05-09 17:55:49 -0700

    Dual Wk 1 V1
    Contains the player variables

--- Wk 1/goal1_assign_duel1/js/mainDualOne.js
+++ Wk 1/goal1_assign_duel1/js/mainDualOne.js
@@ -19,3 +19,19 @@ ELSE IF player two's health is below 1 print "Player one wins"
 return results
 
  */
+
+(function(){
+
+    console.log("FIGHT!!!");
+
+    //player name
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
